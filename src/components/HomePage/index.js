a6484f6605543fa460614fe9e7a1c9c793668716
BuildTools <unconfigured@null.spigotmc.org>
2022-07-20 11:14:25 +0200

    feat: intruducing the  notion of routing

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,15 @@
+// == Style
+import './styles.scss';
+
+// == Component
+import Header from 'src/components/Header';
+
+// == Composant
+function HomePage() {
+  return (
+    <Header />
+  );
+}
+
+// == Export
+export default HomePage;
