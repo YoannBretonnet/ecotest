11e76460301411ea0774f281933425c033584cac
BuildTools <unconfigured@null.spigotmc.org>
2022-07-22 13:54:27 +0200

    feat: file structuration

---
+++ src/actions/mapSettings.js
@@ -0,0 +1,5 @@
+export const OPEN_CLOSE_CAR_MODAL = 'OPEN_CLOSE_CAR_MODAL';
+
+export const openCloseCarModal = () => ({
+  type: OPEN_CLOSE_CAR_MODAL,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authentification from './authentification';
+import mapSettings from './mapSettings';
 
 const rootReducer = combineReducers({
   auth: authentification,
+  mapSettings: mapSettings,
 });
 
 export default rootReducer;
