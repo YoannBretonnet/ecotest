872cccf7f6342c6f945b1261aefeb1c31fcc47f3
BuildTools <unconfigured@null.spigotmc.org>
2022-07-22 13:55:58 +0200

    fix: clear file

--- src/actions/mapSettings.js
+++ src/actions/mapSettings.js
@@ -1,5 +1,1 @@
-export const OPEN_CLOSE_CAR_MODAL = 'OPEN_CLOSE_CAR_MODAL';
 
-export const openCloseCarModal = () => ({
-  type: OPEN_CLOSE_CAR_MODAL,
-});
--- src/reducers/mapSettings.js
+++ src/reducers/mapSettings.js
@@ -1,21 +1,10 @@
-import { OPEN_CLOSE_CAR_MODAL } from 'src/actions/mapSettings';
+import { } from 'src/actions/mapSettings';
 
 export const initialState = {
-  carSettingsModal: {
-    isOpen: false,
-  },
 };
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    case OPEN_CLOSE_CAR_MODAL:
-      return {
-        ...state,
-        carSettingsModal: {
-          ...state.carSettingsModal,
-          isOpen: !state.carSettingsModal.isOpen,
-        },
-      };
     default:
       return state;
   }
