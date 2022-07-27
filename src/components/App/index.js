187b362cb719b481ba89c1b5edb08206fc4c5b5c
BuildTools <unconfigured@null.spigotmc.org>
2022-07-27 13:16:30 +0200

    fix: remove comment in App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,13 +9,6 @@ import HomePage from 'src/components/HomePage';
 
 // == Composant
 function App() {
-  // const [cookies, setCookie, removeCookie] = useCookies();
-  // useEffect(() => {
-  //   setCookie('coucou', 'coucou2', {
-  //     maxAge: 100,
-  //   });
-  // }, []);
-  // console.log(cookies);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
