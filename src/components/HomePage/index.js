d7220787a44d2574419f1cb7c3a8fb86831eb194
BuildTools <unconfigured@null.spigotmc.org>
2022-07-28 00:34:39 +0200

    fix: background image only on Home Page

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -8,11 +8,19 @@ import Main from 'src/components/HomePage/Main';
 import ModalConnection from 'src/components/ModalAuthentification/ModalConnection';
 import ModalAccountCreation from 'src/components/ModalAuthentification/ModalAccountCreation';
 
+import Box from '@mui/material/Box';
+
 // == Composant
 function HomePage() {
   const reducerRoute = 'auth';
   return (
-    <>
+    <Box
+      component="div"
+      id="App"
+      sx={{
+        height: 'fit-content', width: 'fit-content', margin: '0', padding: '0', display: 'flex', flexDirection: 'column', minHeight: '100vh',
+      }}
+    >
       <Header />
       <Main />
       <Footer />
@@ -22,7 +30,7 @@ function HomePage() {
       <ModalAccountCreation
         reducerRoute={reducerRoute}
       />
-    </>
+    </Box>
   );
 }
 
