7237f7450799777b448192d3bc7eac17c44b4c06
BuildTools <unconfigured@null.spigotmc.org>
2022-07-27 12:19:08 +0200

    feat: test react-cookie

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,7 @@
 // == Initialisation
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 
 // == Style
 import './styles.scss';
@@ -9,6 +11,13 @@ import HomePage from 'src/components/HomePage';
 
 // == Composant
 function App() {
+  const [cookies, setCookie, removeCookie] = useCookies();
+  useEffect(() => {
+    setCookie('coucou', 'coucou2', {
+      maxAge: 100,
+    });
+  }, []);
+  console.log(cookies);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
