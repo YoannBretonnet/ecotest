c68bc0315820ff8e5308597f37681900d1394d4a
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-07-28 14:17:46 +0200

    feat: adding pop up click on icon

--- src/components/MapPage/Map/index.js
+++ src/components/MapPage/Map/index.js
@@ -1,8 +1,13 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+// import styles and icons
 import './styles.scss';
-import data from './data.json';
+import icon from "src/assets/images/icon-jumelle.png";
+
+// import data
+import interestPointsData from '../data/interestPointsData.json';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoieWJyZXRvbm5ldCIsImEiOiJjbDVxdXliOHQweHV3M2tvM2hlMG41cXFwIn0.K1s56VTf9EAsagytjhRKSw';
 
@@ -18,16 +23,16 @@ export default function Map() {
   const zoom = 6;
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    // on inititalise la map, centrée entre le point de départ et d'arrivée
+    if (map.current) return; 
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/light-v10',
-      paint: {
-        "background-color": "#6cc573"
-      },
       center: [lng, lat],
       zoom: zoom
-    });
+    });4
+
+    // On ajoute le tracé de la route
     map.current.on('load', () => {
         map.current.addSource('route', {
         'type': 'geojson',
@@ -64,45 +69,65 @@ export default function Map() {
         });
     });
 
+    // On ajoute les points d'intérêt
     map.current.on('load', () => {
-      // Add an image to use as a custom marker
+      // On charge une image
       map.current.loadImage(
-      'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+      icon,
       (error, image) => {
       if (error) throw error;
+      // On ajoute l'image au style en lui donnant un nom d'icone
       map.current.addImage('custom-marker', image);
-      // Add a GeoJSON source with 2 points
-      map.current.addSource('points', 
-      data
+      // On ajoute la source contenant la feature qui utilisera l'icone
+      map.current.addSource('interestPoints', 
+      interestPointsData
       );
        
       // Add a symbol layer
       map.current.addLayer({
-      'id': 'points',
+      'id': 'interestPoints',
       'type': 'symbol',
-      'source': 'points',
+      'source': 'interestPoints',
       'layout': {
-      'icon-image': 'custom-marker',
-      // get the title name from the source's "title" property
-      'text-field': ['get', 'title'],
-      'text-font': [
-      'Open Sans Semibold',
-      'Arial Unicode MS Bold'
-      ],
-      'text-offset': [0, 1.25],
-      'text-anchor': 'top'
+        'icon-image': '{icon}',
+        'icon-allow-overlap': true
       }
       });
       }
       );
       });
+
+      // Quand on clique, ça ouvre une pop-up au niveau des coordonnées du point d'intérêt
+      map.current.on('click', 'interestPoints', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+         
+      //  // On s'assure que le zoom est suffisamment out pour voir toues les points d'intérêt
+      //   while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      //   coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      //   }
+         
+        new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map.current);
+        });
+         
+        // On change le pointeur du curseur
+        map.current.on('mouseenter', 'interestPoints', () => {
+        map.current.getCanvas().style.cursor = 'pointer';
+        });
+         
+        map.current.on('mouseleave', 'interestPoints', () => {
+        map.current.getCanvas().style.cursor = '';
+        });
   });
 
   return (
     <div>
       <div className="sidebar">
-       <div className="details>7 Bornes de recharge | 4 Points d'intéret</div>
-       <div className="sauvegarde>Pour sauvegarder votre trajet, <u>connectez-vous!</u></div>
+       <div className="details">7 Bornes de recharge | 4 Points d'intéret</div>
+       <div className="sauvegarde">Pour sauvegarder votre trajet, <u>connectez-vous!</u></div>
       </div>
       <div ref={mapContainer} className="map-container" />
     </div>
