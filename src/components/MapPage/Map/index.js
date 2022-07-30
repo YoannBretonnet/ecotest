8c0675afe180749b48d0527815923c1b97d88c51
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-07-30 20:31:16 +0200

    fix: refactoring trajet

--- src/components/MapPage/Map/index.js
+++ src/components/MapPage/Map/index.js
@@ -1,15 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import getRoute from './getRoute.js'
+import getRoute from './getRoute'
 
 // import styles and icons
 import './styles.scss';
 
 // import data
 import interestPointsData from '../data/interestPointsData.json';
+import { accessToken } from 'mapbox-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoieWJyZXRvbm5ldCIsImEiOiJjbDVxdXliOHQweHV3M2tvM2hlMG41cXFwIn0.K1s56VTf9EAsagytjhRKSw';
+accessToken = 'pk.eyJ1IjoieWJyZXRvbm5ldCIsImEiOiJjbDVxdXliOHQweHV3M2tvM2hlMG41cXFwIn0.K1s56VTf9EAsagytjhRKSw';
 
 export default function Map() {
   const mapContainer = useRef(null);
@@ -18,79 +19,37 @@ export default function Map() {
   const departureLatitude = useSelector((state) => state.mapData.departureLatitude);
   const arrivalLongitude = useSelector((state) => state.mapData.arrivalLongitude);
   const arrivalLatitude = useSelector((state) => state.mapData.arrivalLatitude);
-  const lng = (departureLongitude + arrivalLongitude)/2;
-  const lat = (departureLatitude+ arrivalLatitude)/2;
+  const lng = (departureLongitude + arrivalLongitude) / 2;
+  const lat = (departureLatitude + arrivalLatitude) / 2;
   const zoom = 6;
 
 
   useEffect(() => {
     // on inititalise la map, centrée entre le point de départ et d'arrivée
-    if (map.current) return; 
+    if (map.current) return;
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/light-v10',
       center: [lng, lat], // starting position
       zoom: 6
     });
-    
+
+    // On récupère les points de départ et d'arrivée
     const start = [-1.54027, 47.21129];
     const end = [-2.00719, 48.63575];
-    const coords = interestPointsData.data.features.map(feature => feature.geometry.coordinates);
-    const coordsString = JSON.stringify(coords);
-    const coordsReplace = coordsString.replaceAll("],[", ";").replace("[[","").replace("]]","");
-    console.log (coordsReplace);
 
-    async function getRoute(end) {
-      // make a directions request using cycling profile
-      // an arbitrary start will always be the same
-      // only the end or destination will change
-      const query = await fetch(
-        `https://api.mapbox.com/directions/v5/mapbox/driving/${start};${coordsReplace};${end}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-        { method: 'GET' }
-      );
-      const json = await query.json();
-      const data = json.routes[0];
-      const route = data.geometry.coordinates;
-      const geojson = {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'LineString',
-          coordinates: route
-        }
-      };
-      // if the route already exists on the map, we'll reset it using setData
-      if (map.current.getSource('route')) {
-        map.current.getSource('route').setData(geojson);
-      }
-      // otherwise, we'll make a new request
-      else {
-        map.current.addLayer({
-          id: 'route',
-          type: 'line',
-          source: {
-            type: 'geojson',
-            data: geojson
-          },
-          layout: {
-            'line-join': 'round',
-            'line-cap': 'round',
-          },
-          paint: {
-            'line-color': '#b6db68',
-            'line-width': 5,
-            'line-opacity': 0.75
-          }
-        }, 'point');
-      }
-    }
-    
+    // On récupère les coordonnées des points d'intérêt
+    const coords = interestPointsData.data.features.map(feature => feature.geometry.coordinates);
+    const coordsReplace = JSON.stringify(coords).replaceAll("],[", ";").replace("[[", "").replace("]]", "");
+
+    // On récupère les coordonnées des balises de recharge
+    // * TO DO *
+
+    // On trace le trajet
     map.current.on('load', () => {
-      // make an initial directions request that
-      // starts and ends at the same location
-      getRoute(end);
-    
-      // Add starting point to the map
+      getRoute(map, start, coordsReplace, end, accessToken);
+
+      // On ajoute les points de départs de d'arrivée
       map.current.addLayer({
         id: 'point',
         type: 'circle',
@@ -123,41 +82,34 @@ export default function Map() {
           'circle-color': '#6cc573'
         }
       });
-
-      
     });
-
-
-
-
-
 
     // On ajoute les points d'intérêt
     map.current.on('load', () => {
-          map.current.addSource('interestPoints', 
-          interestPointsData
-          );
-
-          map.current.addLayer({
-          'id': 'interestPoints',
-          'type': 'symbol',
-          'source': 'interestPoints',
-          'layout': {
-            'icon-image': '{icon}',
-            'icon-allow-overlap': true
-            }
-          });
-       }
+      map.current.addSource('interestPoints',
+        interestPointsData
       );
 
-      // Quand on clique, ça ouvre une pop-up au niveau des coordonnées du point d'intérêt
-      map.current.on('click', 'interestPoints', (e) => {
-        const coordinates = e.features[0].geometry.coordinates.slice();
-        const image = e.features[0].properties.image;
-        const title = e.features[0].properties.title;
-        const description = e.features[0].properties.description;
-         
-        new mapboxgl.Popup()
+      map.current.addLayer({
+        'id': 'interestPoints',
+        'type': 'symbol',
+        'source': 'interestPoints',
+        'layout': {
+          'icon-image': '{icon}',
+          'icon-allow-overlap': true
+        }
+      });
+    }
+    );
+
+    // Quand on clique, ça ouvre une pop-up au niveau des coordonnées du point d'intérêt
+    map.current.on('click', 'interestPoints', (e) => {
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const image = e.features[0].properties.image;
+      const title = e.features[0].properties.title;
+      const description = e.features[0].properties.description;
+
+      new mapboxgl.Popup()
         .setLngLat(coordinates)
         .setHTML(
           `<div>
@@ -165,25 +117,25 @@ export default function Map() {
               <h3>${title}</h3>
               <p>${description}</p>
           </div>`
-          )
+        )
         .addTo(map.current);
-        });
-         
-        // On change le pointeur du curseur
-        map.current.on('mouseenter', 'interestPoints', () => {
-        map.current.getCanvas().style.cursor = 'pointer';
-        });
-         
-        map.current.on('mouseleave', 'interestPoints', () => {
-        map.current.getCanvas().style.cursor = '';
-        });
+    });
+
+    // On change le pointeur du curseur
+    map.current.on('mouseenter', 'interestPoints', () => {
+      map.current.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.current.on('mouseleave', 'interestPoints', () => {
+      map.current.getCanvas().style.cursor = '';
+    });
   });
 
   return (
     <div>
       <div className="sidebar">
-       <div className="details">{interestPointsData.data.features.length} Bornes de recharge | {interestPointsData.data.features.length} Points d'intéret</div>
-       <div className="sauvegarde">Pour sauvegarder votre trajet, <u>connectez-vous!</u></div>
+        <div className="details">{interestPointsData.data.features.length} Bornes de recharge | {interestPointsData.data.features.length} Points d'intéret</div>
+        <div className="sauvegarde">Pour sauvegarder votre trajet, <u>connectez-vous!</u></div>
       </div>
       <div ref={mapContainer} className="map-container" />
     </div>
