9067ab23086cdb72d8b22ea23ae8ffb3693f2464
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-07-31 11:14:59 +0200

    feat: adding isConnected condition to Sidebar text

--- src/components/MapPage/Sidebar/index.js
+++ src/components/MapPage/Sidebar/index.js
@@ -4,12 +4,12 @@ import './styles.scss';
 // import data
 import interestPointsData from '../data/interestPointsData.json';
 
-export default function sidebar() {
+export default function sidebar({text}) {
 
     return (
         <div className="sidebar">
             <div className="details">{interestPointsData.data.features.length} Bornes de recharge | {interestPointsData.data.features.length} Points d'intéret</div>
-            <div className="sauvegarde">Pour sauvegarder votre trajet, <u>connectez-vous!</u></div>
+            <div className="sauvegarde">{text}</div>
         </div>
      
       );
--- src/components/MapPage/index.js
+++ src/components/MapPage/index.js
@@ -7,19 +7,32 @@ import Map from './Map';
 import Sidebar from './Sidebar';
 import ModalConnection from 'src/components/ModalAuthentification/ModalConnection';
 import ModalAccountCreation from 'src/components/ModalAuthentification/ModalAccountCreation';
+import { useSelector } from 'react-redux';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 // == Composant
+
 function MapPage() {
   const reducerRoute = 'auth';
+  const isConnected = useSelector((state) => state.auth.isConnected);
 
   return (
     <>
       <Header />
       <Map />
-      <Sidebar />
+      {
+        !isConnected ? (
+      <Sidebar 
+        text = "Pour sauvegarder votre trajet, connectez-vous!"
+      />
+      ) : (
+        <Sidebar 
+        text = "Sauvegardez ce trajet dans vos favoris"
+      />
+        )
+      }
       <ModalConnection
         reducerRoute={reducerRoute}
       />
