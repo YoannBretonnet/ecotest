4b27cf76e7ad31460a9eef91b74b38308a1b825c
BuildTools <unconfigured@null.spigotmc.org>
2022-08-02 05:08:41 +0200

    feat: start HomePage responsive

--- src/components/HomePage/Main/index.js
+++ src/components/HomePage/Main/index.js
@@ -6,7 +6,9 @@ import {
   Box,
   IconButton,
   Tooltip,
+  useMediaQuery,
 } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { openCloseConnectionModal } from 'src/actions/authentification';
@@ -18,10 +20,13 @@ import {
 } from 'react-icons/bi';
 
 import CarouselComponent from 'src/components/Carousel';
+import StepsComponent from 'src/components/StepsComponent';
 import MenuIsConnnected from 'src/components/MenuIsConnnected';
 
 // == Composant
 function Main() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('mobile'));
   const dispatch = useDispatch();
   const isConnected = useSelector((state) => state.auth.isConnected);
   const { isOpen } = useSelector((state) => state.usability.menu);
@@ -36,21 +41,24 @@ function Main() {
   return (
     <Box component="main" id="main-HomePage">
       <Box component="section" sx={{ margin: '32vh 1.5vh 0' }}>
+        {matches && (
         <h1 className="main-title">
           E-co Roads
         </h1>
-        <p className="main-accroche">
+        )}
+        <p className={matches ? 'main-accroche' : 'main-accroche-desktop'}>
           Découvrez votre région en toute sérénité au volant de votre voiture électrique
         </p>
       </Box>
-      <CarouselComponent />
-      <Box
-        component="section"
-        sx={{
-          position: 'fixed', right: '0', bottom: '0', width: 'fit-content',
-        }}
-      >
-        {
+      {matches ? <CarouselComponent /> : <StepsComponent />}
+      {matches ? (
+        <Box
+          component="section"
+          sx={{
+            position: 'fixed', right: '0', bottom: '0', top: 'unset', width: 'fit-content',
+          }}
+        >
+          {
         !isConnected ? (
           <IconButton
             onClick={() => dispatch(openCloseConnectionModal())}
@@ -75,7 +83,41 @@ function Main() {
           </>
         )
       }
-      </Box>
+        </Box>
+      ) : (
+        <Box
+          component="section"
+          sx={{
+            position: 'fixed', right: '0', bottom: 'unset', top: '0', width: 'fit-content',
+          }}
+        >
+          {
+        !isConnected ? (
+          <IconButton
+            onClick={() => dispatch(openCloseConnectionModal())}
+          >
+            <BiUser size={`${args.size}vh`} />
+          </IconButton>
+        ) : (
+          <>
+            <Tooltip title="Settings">
+              <IconButton
+                onClick={handleClick}
+                aria-controls={isOpen ? 'account-menu' : undefined}
+                aria-haspopup="true"
+              >
+                <BiDotsVerticalRounded size={`${args.size}vh`} />
+              </IconButton>
+            </Tooltip>
+            <MenuIsConnnected
+              inputMenu={inputMenu}
+              setinputMenu={setinputMenu}
+            />
+          </>
+        )
+      }
+        </Box>
+      )}
     </Box>
   );
 }
